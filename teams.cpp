c7557bca0499d74c30e8ad12494879dee46d5db1
Leigh Rubin <leighrub@usc.edu>
2017-01-12 14:07:45 -0800

    Create teams.cpp

---
+++ teams.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// *You* are not allowed to use global variables
+//  but for just the output portion *we* will. ;>
+int combo = 1;
+
+// @brief Prints a single combination of teams
+//
+// @param[in] team1 Array containing the names of team 1
+// @param[in] team2 Array containing the names of team 2
+// @param[in] len Size of each array
+void printSolution(const string* team1, 
+		   const string* team2,
+		   int len)
+{
+  cout << "\nCombination " << combo++ << endl;
+  cout << "T1: ";
+  for(int i=0; i < len; i++){
+    cout << team1[i] << " ";
+  }
+  cout << endl;
+  cout << "T2: ";
+  for(int i=0; i < len; i++){
+    cout << team2[i] << " ";
+  }
+  cout << endl;
+}
+
+// You may add additional functions here
+
+
+
+
+int main(int argc, char* argv[])
+{
+  if(argc < 2){
+    cerr << "Please provide a file of names" << endl;
+    return 1;
+  }
+  // Complete the rest of main
+
+
+
+
+  return 0;
+}
